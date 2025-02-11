bf9e246d4760eb1d513a68868c422bbb43f0e073
Danil Lifantev <d.lifantev@tinkoff.ru>
2025-02-11 20:44:38 +0300

    feat: gsheets version done

--- bot.py
+++ bot.py
@@ -9,15 +9,7 @@ import os
 from dotenv import load_dotenv
 import gspread
 from gspread import Spreadsheet, Worksheet
-from gspread_formatting import (
-    CellFormat,
-    Color,
-    Borders,
-    Border,
-    TextFormat,
-    format_cell_range,
-    set_column_width,
-)
+from gspread_formatting import set_column_width
 from oauth2client.service_account import ServiceAccountCredentials
 
 logging.basicConfig(
@@ -26,42 +18,37 @@ logging.basicConfig(
 
 
 # Headers
-sub_headers = ["Check-in", "Check-out", "Длительность"]
+sub_headers = ["Приход", "Уход", "Итог"]
 
 # Columns
 days_start_col = 4
 
 # Styles
-border_thin = Borders(
-    top=Border(style="SOLID"),
-    bottom=Border(style="SOLID"),
-    left=Border(style="SOLID"),
-    right=Border(style="SOLID"),
-)
-color_grey = Color(red=0.95, green=0.95, blue=0.95)
-color_white = Color(red=1, green=1, blue=1)
+border_thin = {
+    "top": {"style": "SOLID"},
+    "bottom": {"style": "SOLID"},
+    "left": {"style": "SOLID"},
+    "right": {"style": "SOLID"},
+}
+color_grey = {"red": 0.95, "green": 0.95, "blue": 0.95}
+color_white = {"red": 1, "green": 1, "blue": 1}
 
 
-def apply_styles(
-    sheet: Worksheet,
-    cell_range: str,
-    bold=False,
-    color=None,
-):
-    fmt = CellFormat(
-        textFormat=TextFormat(bold=bold),
-        horizontalAlignment="CENTER",
-        borders=border_thin,
-    )
+def format(bold=False, color=None):
+    fmt = {
+        "textFormat": {"bold": bold},
+        "horizontalAlignment": "CENTER",
+        "borders": border_thin,
+    }
     if color:
-        fmt.backgroundColor = color
+        fmt["backgroundColor"] = color
 
-    format_cell_range(sheet, cell_range, fmt)
+    return fmt
 
 
-async def record_attendance(
+def record_attendance(
     action: str, username: str | None, spreadsheet: Spreadsheet
-):
+) -> str:
     now = datetime.now()
     sheet = setup_attendance_sheet(spreadsheet, now)
     user_row = setup_user_row(sheet, username, now)
@@ -73,13 +60,39 @@ async def record_attendance(
     duration_cell = (user_row, col + 2)
 
     if action == "checkin":
-        sheet.update_cell(*checkin_cell, now.strftime("%H:%M"))
+        value = sheet.cell(*checkin_cell).value
+        if value:
+            return f"⚠️ {username}, вы уже отметились сегодня в {value}!"
+
+        checkin_time = now.strftime("%H:%M")
+        sheet.update_cell(*checkin_cell, checkin_time)
+        return f"✅ {username}, ваш вход в {checkin_time} сохранен!"
+
     elif action == "checkout":
-        sheet.update_cell(*checkout_cell, now.strftime("%H:%M"))
+        checkin_time = sheet.cell(*checkin_cell).value
+        if not checkin_time:
+            return f"⚠️ {username}, нет записи о входе сегодня! Пожалуйста, сначала отметьтесь через /checkin."
+
+        checkout_time = sheet.cell(*checkout_cell).value
+        if checkout_time:
+            return (
+                f"⚠️ {username}, вы уже отметились об уходе сегодня в {checkout_time}!"
+            )
+
+        checkout_time_str = now.strftime("%H:%M")
+        sheet.update_cell(*checkout_cell, checkout_time_str)
         sheet.update_cell(
             *duration_cell,
             f"= {col_let(col+1)}{user_row} - {col_let(col)}{user_row}",
         )
+
+        checkin_time = datetime.strptime(checkin_time, "%H:%M")
+        checkout_time = datetime.strptime(checkout_time_str, "%H:%M")
+        duration = checkout_time - checkin_time
+        duration_hours, remain = divmod(duration.seconds, 3600)
+        duration_minutes = remain // 60
+        duration_str = f"{duration_hours:02}:{duration_minutes:02}"
+        return f"❌ {username}, ваш выход в {checkout_time_str} сохранен! Рабочее время: {duration_str}"
 
 
 def setup_attendance_sheet(spreadsheet: Spreadsheet, now: datetime) -> Worksheet:
@@ -92,31 +105,32 @@ def setup_attendance_sheet(spreadsheet: Spreadsheet, now: datetime) -> Worksheet
 
         days_in_month = monthrange(now.year, now.month)[1]
 
-        row1 = ["Username", "Total (1-15)", "Total (16-End)"] + [
-            val for day in range(1, days_in_month + 1) for val in [str(day), "", ""]
+        row1 = ["Имя", "Часы 1-15", "Часы 16-end"] + [
+            val
+            for day in range(1, days_in_month + 1)
+            for val in [f"{day:02}.{now.month:02}", "", ""]
         ]
         row2 = ["", "", ""] + sub_headers * days_in_month
-        # apply_styles(sheet, "A1:C2", bold=True)
-        # set_column_width(sheet, "A:C", 15)
 
         col = days_start_col
-        for _ in range(days_in_month):
+        formats = [{"range": "A1:C2", "format": format(bold=True)}]
+        for i in range(days_in_month):
             col_start = col_let(col)
             col_end = col_let(col + len(sub_headers) - 1)
             sheet.merge_cells(f"{col_start}1:{col_end}1")
 
-            # cell = f"{col_start}1"
-            # color = color_grey if day % 2 == 0 else color_white
-            # apply_styles(sheet, cell, bold=True, color=color)
-            # apply_styles(
-            #     sheet,
-            #     f"{col_let(col)}2:{col_let(col + len(sub_headers)- 1)}2",
-            #     bold=True,
-            #     color=color,
-            # )
+            color = color_grey if i % 2 == 0 else color_white
+            formats.append(
+                {
+                    "range": f"{col_start}1:{col_end}2",
+                    "format": format(bold=True, color=color),
+                }
+            )
             col += len(sub_headers)
 
         sheet.update([row1, row2])
+        set_column_width(sheet, "D:CV", 54)
+        sheet.batch_format(formats)
 
     return sheet
 
@@ -129,21 +143,24 @@ def setup_user_row(sheet: Worksheet, username: str | None, now: datetime) -> int
     user_row = 3 if len(usernames) == 1 else len(usernames) + 1
     sheet.update_cell(user_row, 1, username)
 
-    # apply_styles(sheet, f"A{user_row}:C{user_row}", bold=True)
-
     days_in_month = monthrange(now.year, now.month)[1]
 
-    # col = days_start_col
-    # for day in range(1, days_in_month + 1):
-    #     color = color_grey if day % 2 == 0 else color_white
-    #     cells = (
-    #         f"{col_let(col)}{user_row}:{col_let(col + len(sub_headers) - 1)}{user_row}"
-    #     )
-    #     apply_styles(sheet, cells, color=color)
-    #     col += len(sub_headers)
+    formats = [{"range": f"A{user_row}:C{user_row}", "format": format(bold=True)}]
+    col = days_start_col
+    for i in range(days_in_month):
+        col_start = col_let(col)
+        col_end = col_let(col + len(sub_headers) - 1)
+        color = color_grey if i % 2 == 0 else color_white
+        formats.append(
+            {
+                "range": f"{col_start}{user_row}:{col_end}{user_row}",
+                "format": format(bold=True, color=color),
+            }
+        )
+        col += len(sub_headers)
 
     setup_total_formulas(sheet, user_row, days_in_month)
-
+    sheet.batch_format(formats)
     return user_row
 
 
@@ -173,13 +190,13 @@ class ActionHandler:
 
     async def checkin(self, update: Update, context):
         user = update.effective_user
-        await record_attendance("checkin", user.username, self.spreadsheet)
-        await update.message.reply_text(f"✅ {user.username}, you have checked in!")
+        response = record_attendance("checkin", user.username, self.spreadsheet)
+        await update.message.reply_text(response)
 
     async def checkout(self, update: Update, context):
         user = update.effective_user
-        await record_attendance("checkout", user.username, self.spreadsheet)
-        await update.message.reply_text(f"❌ {user.username}, you have checked out!")
+        response = record_attendance("checkout", user.username, self.spreadsheet)
+        await update.message.reply_text(response)
 
 
 def main():
